c4d948ef50bd3b2ab44a53539149ba1c8ec9083e
meesalji <156134165+meesalji@users.noreply.github.com>
2025-10-06 22:00:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/jahnavi-portfolio/", // must match repo name
+  base: "/jahnavi-portfolio/", // MUST match your repo name
 });
