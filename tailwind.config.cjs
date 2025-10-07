83077dfe5d4acb6244a79379ef1df2612ee59c73
meesalji <156134165+meesalji@users.noreply.github.com>
2025-10-06 21:11:57 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
-// tailwind.config.cjs
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
 };
